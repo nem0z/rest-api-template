26fbc4cb43fff86c7566be82aa4d2502a4de8d4c
nem0z <maxime.bernard.dev@gmail.com>
2022-12-28 01:35:29 +0100

    feat (user): Implement constraints for user models

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,9 +2,22 @@ import { DataTypes } from 'sequelize';
 import { DB } from '../db';
 
 const User = DB.define('User', {
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
+        },
+    },
+    password: {
+        type: DataTypes.STRING(64),
+    },
 });
 
 export default User;
